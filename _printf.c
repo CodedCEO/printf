3b6f43c489ce1a200dca262fcc2e3ad60477c50e
Okeiyi Chidinma <okeiyieunice@gmail.com>
2022-10-19 08:57:40 -0700

    functions

--- _printf.c
+++ _printf.c
@@ -1,143 +1,83 @@
-<<<<<<< HEAD
-#include "main.h"
-
-void print_buffer(char buffer[], int *buff_ind);
-/**
- * _printf - Printf function
- * @format: format.
- * Return: Printed chars.
- */
-int _printf(const char *format, ...)
-{
-	int i, printed = 0, printed_chars = 0;
-	int flags, width, precision, size, buff_ind = 0;
-	va_list list;
-	char buffer[BUFF_SIZE];
-
-	if (format == NULL)
-		return (-1);
-	va_start(list, format);
-	for (i = 0; format && format[i] != '\0'; i++)
-	{
-		if (format[i] != '%')
-		{
-			buffer[buff_ind++] = format[i];
-			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
-			printed_chars++;
-		}
-		else
-		{
-			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
-			i++;
-			printed = handle_print(format, &i, list, buffer,
-					flags, width, precision, size);
-			if (printed == -1)
-				return (-1);
-			printed_chars += printed;
-		}
-	}
-
-	print_buffer(buffer, &buff_ind);
-	va_end(list);
-	return (printed_chars);
-}
-
-/**
- * print_buffer - Prints the contents of the buffer if it exists
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length
- */
-void print_buffer(char buffer[], int *buff_ind)
-{
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
-	*buff_ind = 0;
-=======
 #include <stdarg.h>
-#include <unistd.h>
 #include "main.h"
-
+#include <stddef.h>
 
 /**
- * find_function - function that finds formats for _printf
- * @format: format (char, string, int, decimal)
- * Return: NULL or function associated
+ * get_op - select function for conversion char
+ * @c: char to check
+ * Return: pointer to function
  */
-int (*find_function(const char *format))(va_list)
+int (*get_op(const char c))(va_list)
 {
-	unsigned int i = 0;
-	code_f find_f[] = {
+	int i = 0;
+	flags_p fp[] = {
 		{"c", print_char},
-		{"s", print_string},
-		{"i", print_int},
-		{"d", print_dec},
-		{"r", print_rev},
-		{"b", print_bin},
-		{"u", print_unsig},
+		{"s", print_str},
+		{"i", print_nbr},
+		{"d", print_nbr},
+		{"b", print_binary},
 		{"o", print_octal},
-		{"x", print_x},
-		{"X", print_X},
+		{"x", print_hexa_lower},
+		{"X", print_hexa_upper},
+		{"u", print_unsigned},
+		{"S", print_str_unprintable},
+		{"r", print_str_reverse},
+		{"p", print_ptr},
 		{"R", print_rot13},
-		{NULL, NULL}
+		{"%", print_percent}
 	};
 
-	while (find_f[i].sc)
+	while (i < 14)
 	{
-		if (find_f[i].sc[0] == (*format))
-			return (find_f[i].f);
+		if (c == fp[i].c[0])
+		{
+			return (fp[i].f);
+		}
 		i++;
 	}
 	return (NULL);
 }
+
 /**
- * _printf - function that produces output according to a format
- * @format: format (char, string, int, decimal)
- * Return: size the output text;
+ * _printf - Reproduce behavior of printf function
+ * @format: format string
+ * Return: value of printed chars
  */
 int _printf(const char *format, ...)
 {
 	va_list ap;
-	int (*f)(va_list);
-	unsigned int i = 0, cprint = 0;
+	int sum = 0, i = 0;
+	int (*func)();
 
-	if (format == NULL)
+	if (!format || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
 	va_start(ap, format);
-
 	while (format[i])
 	{
-		while (format[i] != '%' && format[i])
+		if (format[i] == '%')
 		{
-
-			_putchar(format[i]);
-			cprint++;
-			i++;
+			if (format[i + 1] != '\0')
+				func = get_op(format[i + 1]);
+			if (func == NULL)
+			{
+				_putchar(format[i]);
+				sum++;
+				i++;
+			}
+			else
+			{
+				sum += func(ap);
+				i += 2;
+				continue;
+			}
 		}
-		if (format[i] == '\0')
-			return (cprint);
-		f = find_function(&format[i + 1]);
-		if (f != NULL)
-		{
-			cprint += f(ap);
-			i += 2;
-			continue;
-		}
-		if (!format[i + 1])
-			return (-1);
-		_putchar(format[i]);
-		cprint++;
-		if (format[i + 1] == '%')
-			i += 2;
 		else
+		{
+			_putchar(format[i]);
+			sum++;
 			i++;
+		}
 	}
 	va_end(ap);
-	return (cprint);
->>>>>>> 5bfcc0d710657110e8d304f13494f650bcbc0a4b
+	return (sum);
 }
