85430b4bca4dbff8ad808519a83bd59503d7c240
Sunmibola <olaniyisunmibola@gmail.com>
2022-10-20 05:34:46 -0700

    fix numbers code

--- numbers.c
+++ numbers.c
@@ -101,25 +101,30 @@ char *_itoa(int prmNumber)
 	number = prmNumber;
 	cLoop = _nbr_len(number);
 	s = malloc(sizeof(char) * cLoop + 1);
+
 	if (s == NULL)
 	{
 		return (NULL);
 	}
+
 	s[cLoop] = '\0';
 
 	if (number == 0)
 	{
-	       s = "0";
+		s = "0";
 	}
+
 	else if (number < 0)
 	{
 		s[0] = '-';
 		number *= -1;
 	}
+
 	while (number)
 	{
 		s[--cLoop] = number % 10 + 48;
 		number /= 10;
 	}
+
 	return (s);
 }
